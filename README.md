702facfddee36adab3f472632e577a8efa3a3be1
Calabonga <dev@calabonga.net>
2022-09-03 17:06:21 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Calabonga.AspNetCore.AppDefinitions
 Application Definitions base classes. The small but very helpful packege that can help you to organize your ASP.NET Core application.
+
+You can find more information in my blog [Nimble Framework](https://www.calabonga.net/blog/post/nimble-framework-v-6-1)
---
+++ src/Calabonga.AspNetCore.AppDefinitions/IAppDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Calabonga.MinimalApi.Definitions;
+
+/// <summary>
+/// Application definition interface abstraction
+/// </summary>
+internal interface IAppDefinition
+{
+    /// <summary>
+    /// Configure services for current application
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+
+    /// <summary>
+    /// Configure application for current application
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="env"></param>
+    void ConfigureApplication(WebApplication app, IWebHostEnvironment env);
+}
